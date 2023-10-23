0064e395b94cdf553f53ebffcdc7450ff05891a0
JOADISON MS <32674418+Joadison@users.noreply.github.com>
2023-10-23 13:16:29 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
+  target: 'experimental-serverless-trace',
+  future: {
+    webpack5: true,
+  },
 };
 
 module.exports = nextConfig;
