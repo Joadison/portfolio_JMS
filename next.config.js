3eb9786baba14386f323cb1995d8752357fa5b46
JOADISON MS <32674418+Joadison@users.noreply.github.com>
2023-10-23 12:35:52 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   distDir: "dis",
   output: "out",
   basePath: "/portfolio_JMS",
-=======
   output: 'export',
->>>>>>> 0da4fd4c7792a5dac715bc945cffa58b973b7d1d
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
